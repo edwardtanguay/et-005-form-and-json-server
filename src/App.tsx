6caf4fea78c0bc666c58c2e87f213999a35f1a4c
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-04 13:37:22 +0100

    showing debugging data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
+import { useState } from 'react';
 import './App.scss';
 
+const _formData = {
+	jobTitle: '',
+	description: '',
+};
+
 function App() {
+	const [formData, setFormData] = useState(_formData);
+
 	return (
 		<div className="App">
 			<h1>Job Site</h1>
-			<p>Welcome to this site.</p>
+
+			<section>
+				<form>
+					<fieldset>
+						<legend>New Job</legend>
+						<div className="row">
+							<label>Job Title</label>
+							<div>
+								<input type="text" />
+							</div>
+						</div>
+
+						<div className="row">
+							<label>Description</label>
+							<div>
+								<textarea />
+							</div>
+						</div>
+					</fieldset>
+				</form>
+
+				<div className="debuggingArea">
+					<pre>{JSON.stringify(formData, null, 2)}</pre>
+				</div>
+			</section>
 		</div>
 	);
 }
